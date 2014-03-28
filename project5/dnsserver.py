1175211346296700a82a25ba18a80e497d532fac
Huang Yaming <yumminhuang@gmail.com>
2014-03-27 23:25:54 -0400

    Update Project 5: Implement DNSServer

--- project5/dnsserver.py
+++ project5/dnsserver.py
@@ -1,5 +1,7 @@
 import getopt
+import random
 import SocketServer
+import socket
 import struct
 import sys
 """
@@ -43,28 +45,138 @@ DNS Answer
 +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
 """
 
+RECORD={'www.abc.com':'1.1.1.1',
+        'www.def.com':'2.2.2.2'}
+
+
 class DNSPacket:
 
-    def __init__(self):
-        self.id = 0
+    def __init__(self, raw_packet):
+        [self.id, self.flags, self.qcount, self.acount, self.nscount,
+         self.arcount] = struct.unpack('>HHHHHH', raw_packet[:12])
+        self.query = DNSQuery(raw_packet[12:])
+        self.answer = None
 
     def build(self):
-        packet = struct.pack('H', self.id)
+        packet = struct.pack('>HHHHHH', self.id, self.flags,
+                            self.qcount, self.acount,
+                            self.nscount, self.arcount)
+        if self.qcount > 0:
+            packet += self.query.create()
+        if self.acount > 0:
+            packet += self.answer.create()
+
         return packet
 
-    def rebuild(self, raw_packet):
-        [self.id] = struct.unpack('H', raw_packet)
+    def setip(self, ip):
+        self.answer = DNSAnswer(ip)
+        self.acount = 1
+        self.flags = 0x8180
 
-port=0
-name=''
+    def debug_print(self):
+        print 'ID: %X\tFlags:%.4X' % (self.id, self.flags)
+        print 'Query Count:%d\tAnswer Count:%d' % (self.qcount, self.acount)
+        if self.qcount > 0:
+            self.query.debug_print()
+        if self.acount > 0:
+            self.answer.debug_print()
 
-if __name__ == '__main__':
-    opts, args = getopt.getopt(sys.argv[1:],'p:n:')
 
+class DNSQuery:
+    def __init__(self, raw):
+        self.parse(raw)
+
+    def create(self, domain):
+        self.qname = domain
+        query = ''.join(chr(len(x)) + x for x in domain.split('.'))
+        query += '\x00'  # add end symbol
+        return query + struct.pack('>HH', self.qtype, self.qclass)
+
+    def parse(self, data):
+        s = data[:-4]
+        ptr = 0
+        temp=[]
+        while(True):
+            count = ord(s[ptr])
+            if count == 0:
+                break
+            ptr += 1
+            temp.append(s[ptr:ptr+count])
+            ptr += count
+        self.qname = '.'.join(temp)
+        [self.qtype, self.qclass] = struct.unpack('>HH', data[-4:])
+
+    def debug_print(self):
+        print '[DEBUG]DNS QUERY'
+        print 'Request:', self.qname
+        print 'Type: %d\tClass: %d' % (self.qtype, self.qclass)
+
+class DNSAnswer:
+    def __init__(self, ip):
+        self.aname = 0xC02C
+        self.atype = 0x0001
+        self.aclass = 0x0001
+        self.ttl = 60  # time to live
+        self.data = ip
+        self.len = 4
+
+    def create(self):
+        ip = socket.inet_aton(self.src)
+        ans = struct.pack('>HHHLH4s', self.aname, self.atype, self.aclass, self.ttl, self.len, ip)
+        return ans
+
+    def debug_print(self):
+        print '[DEBUG]DNS ANSWER'
+        print 'Query:', self.name
+        print 'Type: %d\tClass: %d' % (self.qtype, self.qclass)
+        print 'TTL: %d\tLength: %d' % (self.ttl, self.len)
+        print self.ip
+
+class DNSUDPHandler(SocketServer.BaseRequestHandler):
+    def handle(self):
+        data = self.request[0].strip()
+        sock = self.request[1]
+        packet = DNSPacket(data)
+        packet.debug_print()
+
+        if packet.query.qtype == 1:
+            domain = packet.query.qname
+            if RECORD.__contains__(domain):
+                packet.setip(RECORD[domain])
+                sock.sendto(packet.build(), self.client_address)
+            else:
+                sock.sendto(data, self.client_address)
+        else:
+            sock.sendto(data, self.client_address)
+
+
+class SimpleDNSServer:
+    def __init__(self, port=53):
+        self.name2ip = {}
+        self.port = port
+
+    def start(self):
+        HOST, PORT = 'localhost', self.port
+        server = SocketServer.UDPServer((HOST, PORT), DNSUDPHandler)
+        server.serve_forever()
+
+
+def parse(args):
+    port = 0
+    name = ''
+
+    opts, args = getopt.getopt(args,'p:n:')
     for o, a in opts:
         if o == '-p':
-            port = a
+            port = int(a)
         elif o == '-n':
             name = a
         else:
             print 'Usage: %s -p <port> -n <name>' % sys.argv[0]
+    return port, name
+
+if __name__ == '__main__':
+    port, domain = parse(sys.argv[1:])
+    print port, type(port)
+    server = SimpleDNSServer(port)
+    server.start()
